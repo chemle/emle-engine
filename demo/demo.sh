87126a01f1e1ed93cf8124408c31db72f6c94583
Lester Hedges <lester.hedges@gmail.com>
2023-01-18 23:00:15 +0000

    Add comments.

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,9 +1,19 @@
+# Specify names in topology and coordinate files.
 PARM=adp.parm7
 CRD=adp.rst7
 
+# Remove and re-create the output directory.
 rm -rf output
 mkdir output
+
+# Switch to the output directory.
 cd output
+
+# Job name.
 name=qmmm
+
+# Launch sander.
 sander -O -i ../$name.in -o $name.out -p ../$PARM -c ../$CRD -r $name.ncrst -x $name.nc
+
+# Kill any running mlmm-server processes.
 mlmm-stop
