2151ebd1e9aebb4c14d06330cbaaa562df42d88b
Lester Hedges <lester.hedges@gmail.com>
2023-01-19 12:21:33 +0000

    Fix versioning.

--- mlmm/_version.py
+++ mlmm/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "mlmm-"
     cfg.versionfile_source = "mlmm/_version.py"
     cfg.verbose = False
