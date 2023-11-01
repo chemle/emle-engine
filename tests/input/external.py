fa033ed671640d94f35e06b04c694e300775647f
Lester Hedges <lester.hedges@gmail.com>
2023-11-01 14:38:41 +0000

    Add unit test for external backend.

---
+++ tests/input/external.py
@@ -0,0 +1,44 @@
+import ase
+
+BOHR_TO_ANGSTROM = ase.units.Bohr
+EV_TO_HARTREE = 1.0 / ase.units.Hartree
+
+
+def run_xtb(atoms):
+    """
+    Internal function to compute in vacuo energies and gradients using
+    the xtb-python interface. Currently only uses the "GFN2-xTB" method.
+
+    Parameters
+    ----------
+
+    atoms : ase.atoms.Atoms
+        The atoms in the QM region.
+
+    Returns
+    -------
+
+    energy : float
+        The in vacuo ML energy in Eh.
+
+    gradients : numpy.array
+        The in vacuo gradient in Eh/Bohr.
+    """
+
+    if not isinstance(atoms, ase.Atoms):
+        raise TypeError("'atoms' must be of type 'ase.atoms.Atoms'")
+
+    from xtb.ase.calculator import XTB
+
+    # Create the calculator.
+    atoms.calc = XTB(method="GFN2-xTB")
+
+    # Get the energy and forces in atomic units.
+    energy = atoms.get_potential_energy()
+    forces = atoms.get_forces()
+
+    # Convert to Hartree and Eh/Bohr.
+    energy *= EV_TO_HARTREE
+    gradient = -forces * EV_TO_HARTREE * BOHR_TO_ANGSTROM
+
+    return energy, gradient
