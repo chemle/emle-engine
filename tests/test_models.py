1e7281f825217c1b99f9884962a8376d73b87e0b
Lester Hedges <lester.hedges@gmail.com>
2024-08-14 14:56:54 +0100

    Test that models can calculate energies and gradients.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,50 @@
+import numpy
 import pytest
 import torch
 
 from emle.models import *
+
+dtype = torch.float32
+device = torch.get_default_device()
+
+
+@pytest.fixture(scope="module")
+def atomic_numbers():
+    return torch.tensor(
+        numpy.load("tests/input/atomic_numbers.npy"),
+        dtype=torch.int64,
+        device=device,
+    )
+
+
+@pytest.fixture(scope="module")
+def charges_mm():
+    return torch.tensor(
+        numpy.load("tests/input/charges_mm.npy"),
+        dtype=dtype,
+        device=device,
+    )
+
+
+@pytest.fixture(scope="module")
+def xyz_qm():
+    return torch.tensor(
+        numpy.load("tests/input/xyz_qm.npy"),
+        dtype=dtype,
+        device=device,
+        requires_grad=True,
+    )
+
+
+@pytest.fixture(scope="module")
+def xyz_mm():
+    return torch.tensor(
+        numpy.load("tests/input/xyz_mm.npy"),
+        dtype=dtype,
+        device=device,
+        requires_grad=True,
+    )
+
 
 try:
     import NNPOps
@@ -26,26 +69,28 @@ except:
 
 
 @pytest.mark.parametrize("alpha_mode", ["species", "reference"])
-def test_emle(alpha_mode):
+def test_emle(alpha_mode, atomic_numbers, charges_mm, xyz_qm, xyz_mm):
     """
     Check that we can instantiate the default EMLE model.
     """
     # Instantiate the default EMLE model.
     model = EMLE(alpha_mode=alpha_mode)
-    assert model is not None
 
     # Make sure the model can be converted to TorchScript.
     model = torch.jit.script(model)
 
+    # Get the energy and gradients.
+    energy = model(atomic_numbers, charges_mm, xyz_qm, xyz_mm)
+    grad_qm, grad_mm = torch.autograd.grad(energy.sum(), (xyz_qm, xyz_mm))
+
 
 @pytest.mark.parametrize("alpha_mode", ["species", "reference"])
-def test_ani2x(alpha_mode):
+def test_ani2x(alpha_mode, atomic_numbers, charges_mm, xyz_qm, xyz_mm):
     """
     Check that we can instantiate the default ANI2xEMLE model.
     """
     # Instantiate the ANI2xEMLE model.
     model = ANI2xEMLE(alpha_mode=alpha_mode)
-    assert model is not None
 
     # Make sure the model can be converted to TorchScript.
     model = torch.jit.script(model)
@@ -61,20 +106,26 @@ def test_ani2x(alpha_mode):
     # Make sure the model can be converted to TorchScript.
     model = torch.jit.script(model)
 
+    # Get the energy and gradients.
+    energy = model(atomic_numbers, charges_mm, xyz_qm, xyz_mm)
+    grad_qm, grad_mm = torch.autograd.grad(energy.sum(), (xyz_qm, xyz_mm))
+
 
 @pytest.mark.skipif(not has_nnpops, reason="NNPOps not installed")
 @pytest.mark.parametrize("alpha_mode", ["species", "reference"])
-def test_ani2x_nnpops(alpha_mode):
+def test_ani2x_nnpops(alpha_mode, atomic_numbers, charges_mm, xyz_qm, xyz_mm):
     """
     Check that we can instantiate the default ANI2xEMLE model with NNPOps.
     """
     # Instantiate the ANI2xEMLE model using NNPOps.
-    atomic_numbers = torch.tensor([1, 6, 7, 8])
     model = ANI2xEMLE(alpha_mode=alpha_mode, atomic_numbers=atomic_numbers)
-    assert model is not None
 
     # Make sure the model can be converted to TorchScript.
     model = torch.jit.script(model)
+
+    # Get the energy and gradients.
+    energy = model(atomic_numbers, charges_mm, xyz_qm, xyz_mm)
+    grad_qm, grad_mm = torch.autograd.grad(energy.sum(), (xyz_qm, xyz_mm))
 
 
 @pytest.mark.skipif(not has_mace, reason="mace-torch not installed")
@@ -83,13 +134,16 @@ def test_ani2x_nnpops(alpha_mode):
 @pytest.mark.parametrize(
     "mace_model", ["mace-off23-small", "mace-off23-medium", "mace-off23-large"]
 )
-def test_mace(alpha_mode, mace_model):
+def test_mace(alpha_mode, mace_model, atomic_numbers, charges_mm, xyz_qm, xyz_mm):
     """
     Check that we can instantiate the default MACE model.
     """
     # Instantiate the MACEEMLE model.
     model = MACEEMLE(alpha_mode=alpha_mode)
-    assert model is not None
 
     # Make sure the model can be converted to TorchScript.
     model = torch.jit.script(model)
+
+    # Get the energy and gradients.
+    energy = model(atomic_numbers, charges_mm, xyz_qm, xyz_mm)
+    grad_qm, grad_mm = torch.autograd.grad(energy.sum(), (xyz_qm, xyz_mm))
