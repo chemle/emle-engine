aa83fb7d2f8bd71fa42ba2888e2c7f486bc87236
Kirill Zinovjev <kzinovjev@gmail.com>
2025-04-13 10:01:54 +0200

    Add energy deviation to QbC deviations file

--- emle/calculator.py
+++ emle/calculator.py
@@ -1309,9 +1309,10 @@ class EMLECalculator:
                 raise RuntimeError(msg)
 
             if (self._qbc_deviation):
+                E_std = _torch.std(base_model._E_vac_qbc).item()
                 max_f_std = _torch.max(_torch.std(base_model._grads_qbc, axis=0)).item()
                 with open(self._qbc_deviation, "a") as f:
-                    f.write(f"{max_f_std:12.5f}\n")
+                    f.write(f"{E_std:12.5f}{max_f_std:12.5f}\n")
                 if self._qbc_deviation_threshold and max_f_std > self._qbc_deviation_threshold:
                     msg = "Force deviation threshold reached!"
                     raise ValueError(msg)
